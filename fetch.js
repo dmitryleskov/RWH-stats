d13177d951d40df667a8081e4beff21e401fa1d5
Dmitry Leskov <git@dmitryleskov.com>
2013-08-24 16:02:27 +0700

    Retrieve one chapter, count comments

--- fetch.js
+++ fetch.js
@@ -1,6 +1,5 @@
 var page = require('webpage').create(),
-    system = require('system'),
-    t, address;
+    system = require('system');
 
 if (system.args.length === 1) {
     console.log('Usage: fetch.js <some URL>');
@@ -8,18 +7,56 @@ if (system.args.length === 1) {
 }
 
 address = system.args[1];
+
+page.onResourceRequested = function (requestData, networkRequest) {
+    console.log("Requested " + requestData.url + ", id=" + requestData.id)
+}
+page.onResourceReceived = function (response) {
+    if (response.stage === 'end') console.log("Received " + response.id)
+}
+
 page.open(address, function (status) {
     if (status !== 'success') {
         console.log('FAIL to load the address');
+        phantom.exit();
     } else {
-        var text = page.evaluate(function() {
+        var chapters = page.evaluate(function() {
+            var TEXT_NODE = 3;
             var hrefs = [];
+            var chtp = /^\d+/;
             $('[class^="zebra_"] > a').each(function() {
-                hrefs.push($(this).attr('href'));
+                if (this.previousSibling &&
+                    this.previousSibling.nodeType == TEXT_NODE &&
+                    chtp.test(this.previousSibling.textContent)) {
+                    hrefs.push($(this).attr('href'));
+                }
             })
-            return hrefs.length;
+            return hrefs;
         });
-        console.log(text);
+        console.log(chapters);
+        console.log(chapters instanceof Array);
+        page.open(address+"/"+chapters[0], function (status) {
+            if (status !== 'success') {
+                console.log('Failed to retrieve chapter');
+                phantom.exit();
+            } else {
+                setTimeout(function () {
+                    var comments = page.evaluate(function () {
+                        var count = 0
+                        $('.comment').each(function() {
+                            var num = parseInt($(this).text())
+                            if (!isNaN(num)) count += num
+                        })
+                        return count
+                    })
+                    console.log("Total number of comments: " + comments)
+                    console.log('Initiate comment loading')
+                    page.evaluate(function () {
+                        loadAllComments()
+                    })
+//                    phantom.exit();
+                }, 3000)
+            }
+        })
     }
-    phantom.exit();
 });
